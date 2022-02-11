314d27085d5278e655563985dae8051ffe87c114
Thibault Goudouneix <nilmanduil@gmail.com>
2022-02-11 18:46:56 +0100

    feat: initialize __run.js for Storybook (WIP)

---
+++ projects/svelte-add/adders/storybook/__run.js
@@ -0,0 +1,11 @@
+import { walk } from "estree-walker";
+import { addImport, findImport, setDefaultDefaultExport, setDefault, setPropertyValue } from "../../ast-tools.js";
+import { storybookFolder } from "./stuff.js";
+
+/__info.js").Options>} */;
+export const run = async ({ folderInfo, install, runCommand, updateJavaScript, updateJson, applyPreset }) => {
+	// TODO 1: run `npx sb init`
+	// TODO 2: move `.storybook/main.js` to `.storybook/main.cjs`
+	// TODO 3: duplicate svelte-preprocess config into Storybook
+	// TODO 4 : initialize `.storybook/package.json` with an empty object
+};
